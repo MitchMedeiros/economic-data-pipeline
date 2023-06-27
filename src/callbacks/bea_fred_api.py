a4b90e0641c6afad4ae8a210b32962b589016171
Mitchell-Medeiros <mitch_med@yahoo.com>
2023-06-27 12:42:05 -0500

    Partly refactored the bea and fred api callback.

--- src/callbacks/bea_fred_api.py
+++ src/callbacks/bea_fred_api.py
@@ -18,10 +18,16 @@ class RestAPI:
             response = requests.get(url)
             if response.status_code == 200: self.data[key] = response.json()
 
-'''
-BEA Endpoint Descriptions:
-"gdp" - Percent Change From Preceding Period in Real Gross Domestic Product (A) (Q).
-'''
+def add_date_table(table, dataset_list):
+    date_table_added = table not in dataset_list
+    if date_table_added: dataset_list += [table]
+    return date_table_added
+
+def create_date_series(date_name, data):
+    periods = [item[date_name] for item in data]
+    unique_periods = list(set(periods))
+    date_sr = pd.Series(unique_periods, name='date').sort_values().reset_index(drop=True)
+    return date_sr
 
 def bea_fred_callback(app):
     @app.callback(
@@ -38,16 +44,11 @@ def bea_fred_callback(app):
         all_years_string = range(start_year, end_year + 1)
         all_years_string = ','.join(str(year) for year in all_years_string)
 
-        if 'T10101' not in selected_bea_tables: 
-            selected_bea_tables += ['T10101']
-            T10101_added = True
-        else: 
-            T10101_added = False
+        add_bea_date_table = add_date_table('T10101', selected_bea_tables)
 
-        bea_base_url = f"https://apps.bea.gov/api/data/"
+        BEA_BASE_URL = f"https://apps.bea.gov/api/data/"
 
-        # The multiselect components' value: datasets, is a list of table names.
-        # We can use dictionary comprehension to create the endpoints from this, with the table names are used as the keys.
+        # The multiselect components' value: datasets, is a list of table names. This is converted to a dictionary of API endpoints.
         bea_endpoints = {
             f'{table}': (f"?&UserID={my_config.BEA_KEY}"
                         f"&method=GetData"
@@ -64,60 +65,15 @@ def bea_fred_callback(app):
             "T10101": "Gross domestic product",
             'T10105': "Gross domestic product",
             'T10107': "Gross domestic product",
-            'T20100': "Personal income",
-            'T20200B': "Wages and salaries",
-            'T61600A': "test"
+            'T20100': "Personal income"
         }
 
-        bea_api = RestAPI(bea_base_url, bea_endpoints)
-        bea_api.fetch_data()
-
-        # Creating a date series to index the user selected DataFrames.
-        periods = [item['TimePeriod'] for item in bea_api.data['T10101']['BEAAPI']['Results']['Data']]
-        unique_periods = list(set(periods))
-        date_sr = pd.Series(unique_periods, name='date').sort_values().reset_index(drop=True)
-
-        if T10101_added: selected_bea_tables.remove('T10101')
-
-        # Replacing the JSON data within the bea_api.data dictionary with formatted DataFrames.
-        all_dfs = [date_sr]
-        for table in selected_bea_tables:
-            try:
-                bea_api.data[table] = (
-                    pd.DataFrame(bea_api.data[table]['BEAAPI']['Results']['Data'], columns=['DataValue', 'METRIC_NAME', 'LineDescription']) \
-                    .rename(columns={'DataValue': table + '_value', 'METRIC_NAME': table + '_metric'}))
-            except KeyError:
-                return dmc.Alert(
-                    title="Invalid Years: No data is available within the selected years for one of the requested datasets.",
-                    icon=DashIconify(icon='mingcute:alert-fill'),
-                    color='yellow',
-                    withCloseButton=True,
-                ), False
-            bea_api.data[table] = (
-                bea_api.data[table].loc[bea_api.data[table]['LineDescription'] == filter_metrics[table]]
-                .drop(columns=['LineDescription']))
-
-            # The tables contain multiple values for a given economic measure using differnt methodologies. These are vertically stacked.
-            # To avoid needing to filter on the method column, the table is pivotted in a way that doesn't produce NaN values:
-            sliced_dfs = []
-            unique_metrics = bea_api.data[table][table + '_metric'].unique()
-            for metric in unique_metrics:
-                sliced_df = (
-                    bea_api.data[table].loc[bea_api.data[table][table + '_metric'] == metric]
-                    .drop(columns=[table + '_metric'])
-                    .reset_index(drop=True)
-                    .rename(columns={f'{table}_value': f'{table} - ' + metric})
-                )
-                sliced_dfs.append(sliced_df)
-            merged_df = pd.concat(sliced_dfs, axis=1)
-            all_dfs.append(merged_df)
-
-        fred_units = "pch"
-        fred_frequency = "q"
+        FRED_UNITS = "pch"
+        FRED_FREQUENCY = "q"
         fred_start_year = f"{start_year}-01-01"
         fred_end_year = f"{end_year}-01-01"
 
-        fred_base_url = "https://api.stlouisfed.org/fred/series/observations"
+        FRED_BASE_URL = "https://api.stlouisfed.org/fred/series/observations"
 
         fred_endpoints = {
             f'{table}': (f"?series_id={table}"
@@ -125,13 +81,54 @@ def bea_fred_callback(app):
                             f"&file_type=json"
                             f"&observation_start={fred_start_year}"
                             f"&observation_end={fred_end_year}"
-                            f"&units={fred_units}"
-                            f"&frequency={fred_frequency}"
+                            f"&units={FRED_UNITS}"
+                            f"&frequency={FRED_FREQUENCY}"
                             f"&aggregation_method=sum")
             for table in selected_fred_tables
         }
+
+        bea_api = RestAPI(BEA_BASE_URL, bea_endpoints)
+        bea_api.fetch_data()
+
+        date_sr = create_date_series('TimePeriod', bea_api.data['T10101']['BEAAPI']['Results']['Data'])
+
+        if add_bea_date_table: selected_bea_tables.remove('T10101')
+
+        def process_bea_table(bea_api, table, filter_metrics):
+            try:
+                data = bea_api.data[table]['BEAAPI']['Results']['Data']
+                df = pd.DataFrame(data, columns=['DataValue', 'METRIC_NAME', 'LineDescription'])
+            except KeyError:
+                return dmc.Alert(
+                    title="Invalid Years: No data is available within the selected years for one of the requested datasets.",
+                    icon=DashIconify(icon='mingcute:alert-fill'),
+                    color='yellow',
+                    withCloseButton=True,
+                ), False
+
+            if table != 'T20307':
+                df = df.loc[df['LineDescription'] == filter_metrics[table]].drop(columns=['LineDescription'])
+
+            sliced_dfs = []
+            unique_metrics = df['METRIC_NAME'].unique()
+            for metric in unique_metrics:
+                sliced_df = df.loc[df['METRIC_NAME'] == metric] \
+                    .drop(columns=['METRIC_NAME']) \
+                    .reset_index(drop=True) \
+                    .rename(columns={'DataValue': f'{table} - ' + metric})
+                sliced_dfs.append(sliced_df)
+
+            pivoted_df = pd.concat(sliced_dfs, axis=1)
+            return pivoted_df
+
+        all_dfs = [date_sr]
+        for table in selected_bea_tables:
+            bea_df = process_bea_table(bea_api, table, filter_metrics)
+            if bea_df is not None:
+                all_dfs.append(bea_df)
+
         
-        fred_api = RestAPI(fred_base_url, fred_endpoints)
+        fred_api = RestAPI(FRED_BASE_URL, fred_endpoints)
         fred_api.fetch_data()
 
         for table in selected_fred_tables:
